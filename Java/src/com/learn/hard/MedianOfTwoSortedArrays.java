3040c0ba772d1ebf1b28032e9b5fd6257c117ac6
Yinevg <864248425@qq.com>
2018-11-09 22:44:26 +0800

    hard-4.MedianOfTwoSortedArrays

---
+++ Java/src/com/learn/hard/MedianOfTwoSortedArrays.java
@@ -0,0 +1,38 @@
+package com.learn.hard;
+
+/**
+ * Created by Yinevg on 2018/11/9
+ * https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+ * <p>
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+ * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+ * You may assume nums1 and nums2 cannot be both empty.
+ * <p>
+ * Example 1:
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ * The median is 2.0
+ * <p>
+ * Example 2:
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ * The median is (2 + 3)/2 = 2.5
+ */
+public class MedianOfTwoSortedArrays {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] nums = new int[nums1.length + nums2.length];
+        int i = 0, j = 0, k = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) nums[k++] = nums1[i++];
+            else nums[k++] = nums2[j++];
+        }
+        while (i < nums1.length) {
+            nums[k++] = nums1[i++];
+        }
+        while (j < nums2.length) {
+            nums[k++] = nums2[j++];
+        }
+        if (k % 2 == 0) return (nums[k / 2] + nums[k / 2 - 1]) / 2.0;
+        else return nums[k / 2];
+    }
+}
