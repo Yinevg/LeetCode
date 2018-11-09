b98de9860e8dd91885bce6b2d6b31fdcf0b34659
Yinevg <864248425@qq.com>
2018-11-09 22:59:36 +0800

    Update TwoSum.py

--- Python3/easy/TwoSum.py
+++ Python3/easy/TwoSum.py
@@ -10,4 +10,4 @@ class Solution:
             if nums[i] in buff_dict:
                 return [buff_dict[nums[i]], i]
             else:
-                buff_dict[targe - nums[i]] = i
+                buff_dict[target - nums[i]] = i
