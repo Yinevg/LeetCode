6bb9eedabaebfd03429565fa25b5559ee29e4fb7
Yinevg <864248425@qq.com>
2019-06-26 21:42:01 +0800

    100 SameTree java

---
+++ Java/src/com/learn/easy/SameTree.java
@@ -0,0 +1,53 @@
+package com.learn.easy;
+
+import com.learn.object.TreeNode;
+
+/**
+ * Created by Yinevg on 2019/6/26
+ * Given two binary trees, write a function to check if they are the same or not.
+ * <p>
+ * Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:     1         1
+ *          / \       / \
+ *        2   3     2   3
+ * <p>
+ * [1,2,3],   [1,2,3]
+ * <p>
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input:     1         1
+ *          /           \
+ *        2             2
+ * <p>
+ * [1,2],     [1,null,2]
+ * <p>
+ * Output: false
+ * Example 3:
+ * <p>
+ * Input:     1         1
+ *          / \       / \
+ *         2   1     1   2
+ * <p>
+ * [1,2,1],   [1,1,2]
+ * <p>
+ * Output: false
+ * Accepted
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
